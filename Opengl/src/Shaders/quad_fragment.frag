7517671b273057a4c9262a50e07d86a13071efd0
kodzimk <proggamedev@gmail.com>
2024-02-15 20:59:31 +0600

    creating shadow again and post rocessign effects

--- Opengl/src/Shaders/quad_fragment.frag
+++ Opengl/src/Shaders/quad_fragment.frag
@@ -5,11 +5,31 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
+out vec3 crnt;
+
 uniform sampler2D tex_u;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+
 
 void main()
-{             
+{           
+    float ambient = 0.20f;
+    vec3 normal = normalize(Normal);
+    vec3 lightDirection = normalize(lightPos - crnt);
+    float diffuse = max(dot(normal,lightDirection),0.0);
 
-    FragColor = texture(tex_u,TexCoords);
+    float specular = 0.0f;
 
+    if(diffuse != 0.0f)
+    {
+        float specPower = 0.5f;
+        vec3 viewDirection = normalize(viewPos - crnt);
+        vec3 halfWayVec = normalize(viewDirection+lightDirection);
+        float specAmount = pow(max(dot(normal,halfWayVec),0.0),16);
+        specular = specAmount * specPower;
+    }
+
+    FragColor.rgb = texture(tex_u,TexCoords).rgb*(diffuse+specular+ambient);
 }
--- Opengl/src/main.cpp
+++ Opengl/src/main.cpp
@@ -45,11 +45,36 @@ int main(void)
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
     glEnableVertexAttribArray(1);
 
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
+    glEnableVertexAttribArray(2);
+
+
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)(sizeof(float) * 2));
+    glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     unsigned int program = CreateProgram("src/Shaders/vertex_shader.vert", "src/Shaders/fragment_shader.frag");
-    unsigned int quadProgram = CreateProgram("src/Shaders/quad_vertex.vert", "src/Shaders/quad_fragment.frag");
+    unsigned int shadowMap = CreateProgram("src/Shaders/shadow_vertex.vert", "src/Shaders/shadow_fragment.frag");
+    unsigned int frameBufferProgram = CreateProgram("src/Shaders/framebuffer_vertex.vert", "src/Shaders/framebuffer_fragment.frag");
 
     int width, height, channels;
     stbi_set_flip_vertically_on_load(true);
@@ -98,6 +123,8 @@ int main(void)
     stbi_image_free(data);
 
 
+
+
     glm::mat4 blockMatrix = glm::mat4(1.0f);
     blockMatrix = glm::translate(blockMatrix, glm::vec3(0.0f, 1.0f, 0.0f));
 
@@ -105,7 +132,7 @@ int main(void)
     quadMatrix = glm::translate(quadMatrix, glm::vec3(0.0f,-1.0f,0.0f));
 
     glm::mat4 quadMatrix2 = glm::mat4(1.0f);
-    quadMatrix2 = glm::translate(quadMatrix, glm::vec3(0.0f, 1.0f, 0.0f));
+    quadMatrix2 = glm::translate(quadMatrix, glm::vec3(2.0f, 1.0f, 0.0f));
 
     Camera camera(SRC_WIDTH,SRC_HEIGHT,glm::vec3(0.0f,2.0f,2.0f),0.0f);
 
@@ -113,8 +140,50 @@ int main(void)
     glUniform3f(glGetUniformLocation(program, "lightPos"), 0.0f, 0.0f, -1.0f);
     glUniform1i(glGetUniformLocation(program, "diffuseTexture"), 1);
 
-    glUseProgram(quadProgram);
-    glUniform1i(glGetUniformLocation(quadProgram, "tex_u"), 0);
+    glUseProgram(frameBufferProgram);
+    glUniform1i(glGetUniformLocation(frameBufferProgram, "screenTexture"), 2);
+
+
+    unsigned int FBO;
+    glGenFramebuffers(1, &FBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+    unsigned int fboTexture;
+    glGenTextures(1, &fboTexture);
+    glBindTexture(GL_TEXTURE_2D, fboTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SRC_WIDTH, SRC_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fboTexture, NULL);
+
+    unsigned int RBO;
+    glGenRenderbuffers(1, &RBO);
+    glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SRC_WIDTH, SRC_HEIGHT);
+    glFramebufferRenderbuffer(GL_TEXTURE_2D, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    unsigned int SHADOW_WIDTH = 2048, SHADOW_HEIGHT = 2048;
+
+    unsigned int depthMapFBO;
+    glGenFramebuffers(1, &depthMapFBO);
+    
+    unsigned int depthMapTexture;
+    glGenTextures(1, &depthMapTexture);
+    glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMapTexture, 0);
+    glReadBuffer(GL_NONE);
+    glDrawBuffer(GL_NONE);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
     STATE state = STATE::IDLE;
     Obj_Loader obj;
@@ -126,13 +195,11 @@ int main(void)
         camera.Inputs(window);
         camera.updateMatrix(45.f, 0.1f, 100.f);
 
+        glBindFramebuffer(GL_FRAMEBUFFER, FBO);
         glEnable(GL_DEPTH_TEST);
        
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-
-        std::cout << collision(glm::vec3(0.0f, 1.0f,0.0f), glm::vec3(0.0f, -1.0f, 0.0f),1.0f,1.0f,1.0f,1.0f,1.0f,1.0f) << std::endl;
-
 
         glUseProgram(program);
         glBindVertexArray(obj.VAOs[0]);
@@ -143,21 +210,26 @@ int main(void)
         glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(blockMatrix));
         glDrawArrays(GL_TRIANGLES, 0, obj.size);
 
-        glUseProgram(quadProgram);
+    
         glBindVertexArray(VAO);
-        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, container);
-        camera.Matrix(quadProgram, "playerMatrix");
-        glUniformMatrix4fv(glGetUniformLocation(quadProgram, "model"), 1, GL_FALSE, glm::value_ptr(quadMatrix));
+        camera.Matrix(program, "playerMatrix");
+        glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(quadMatrix));
         glDrawArrays(GL_TRIANGLES, 0, 110);
 
-        glUseProgram(quadProgram);
         glBindVertexArray(VAO);
-        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, container);
-        camera.Matrix(quadProgram, "playerMatrix");
-        glUniformMatrix4fv(glGetUniformLocation(quadProgram, "model"), 1, GL_FALSE, glm::value_ptr(quadMatrix2));
+        camera.Matrix(program, "playerMatrix");
+        glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(quadMatrix2));
         glDrawArrays(GL_TRIANGLES, 0, 110);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glDisable(GL_DEPTH_TEST);
+        glUseProgram(frameBufferProgram);
+        glBindVertexArray(quadVAO);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, fboTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
      
         glfwSwapBuffers(window);
 
